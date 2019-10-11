c2915549571e44bdd2d08d361ff541a780112121
Koling <32242912+Koling@users.noreply.github.com>
2019-10-11 21:21:16 +0300

    Update TeamProject.py

--- TeamProject.py
+++ TeamProject.py
@@ -1,10 +1,14 @@
-a = int(input())
-for k in range(a):
-    if k < 10:
-        print(k-1)
-    else:
-        break
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.core.window import Window
 
-print("Kek")
-print('Tessting')
-print('Пацаны,это тот файл который мы будем редачить!')
+from kivy.config import Config
+Config.set('graphics', 'width', '200')
+Config.set('graphics', 'height', '200')
+
+class MyApp(App):
+    def build(self):
+        return Label(text='text')
+
+if __name__ == '__main__':
+    MyApp().run()
